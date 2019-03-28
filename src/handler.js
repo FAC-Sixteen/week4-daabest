50c539f8977c8ec1a83e028cb1c64764338d19fa
Ania Blaziak <anblaziak@gmail.com>
2019-03-28 14:52:49 +0000

    DOM is getting the user input value

--- src/handler.js
+++ src/handler.js
@@ -8,6 +8,7 @@ function handler(request, response) {
         html: "text/html",
         css: "text/css",
         js: "application/javascript",
+        js: "text/javascript",
         jpg: "image/jpg",
         png: "image/png",
         ico: "image/x-icon"
@@ -15,26 +16,30 @@ function handler(request, response) {
 
     if (url === "/") {
         const filePath = path.join(__dirname + '/..' + "/public" + "/index.html")
-        fs.readFile(filePath, function(error, file){
-            if(error){
+        fs.readFile(filePath, function (error, file) {
+            if (error) {
                 response.write("Sorry, there was a problem on our end.");
                 return;
             }
-            response.writeHead(200, {"Content-Type": "text/html"});
-            response.end(file);
+            response.writeHead(200, {
+                "Content-Type": "text/html"
             });
+            response.end(file);
+        });
     } else {
         const filePath = path.join(__dirname + '/..' + "/public") + url
-        fs.readFile(filePath, function(error, file){
-            if(error){
+        fs.readFile(filePath, function (error, file) {
+            if (error) {
                 response.write("Sorry, there was a problem on our end.");
                 return;
             }
-            response.writeHead(200, {"Content-Type": extensionType[extension]});
-            response.end(file);
+            response.writeHead(200, {
+                "Content-Type": extensionType[extension]
             });
+            response.end(file);
+        });
     }
-        
-    }
+
+}
 
 module.exports = handler;
